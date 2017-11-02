3c53590702011c8d2488da87b2a108ca977e2e26
anthonymuo <a.c.muo.16@aberdeen.ac.uk>
2017-11-02 12:37:02 +0000

    updated

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,9 +6,20 @@ module SessionsHelper
   end
 
   # Returns the current logged-in user (if any).
+  
+# Returns the user corresponding to the remember token cookie.
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+   if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+   end
   end
+
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
@@ -22,3 +33,4 @@ module SessionsHelper
     @current_user = nil
   end
 end
+
